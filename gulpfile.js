5dd8409ebab02c239c5801c06ae6072cc3ea5078
Jacob Blakely <jacob@codekirei.com>
2016-01-19 13:43:36 -0800

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ const source = require('vinyl-source-stream')
 const gutil = require('gulp-util')
 const autoImport = require('auto-import')
 const babelify = require('babelify')
+const buffer = require('vinyl-buffer')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
 
 //----------------------------------------------------------
 // fns
@@ -18,6 +21,9 @@ const babelify = require('babelify')
 const browserifyStream = browserify(
   { entries: ['src/app.js']
   , transform: [babelify.configure({presets: ['es2015']})]
+  , cache: {}
+  , packageCache: {}
+  , debug: true
   })
 
 browserifyStream.on('log', txt => gutil.log(`Browserify: ${txt}`))
@@ -26,6 +32,10 @@ const consume = stream => {
   stream.bundle()
     .on('error', err => console.log(err.stack))
     .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'))
 }
 
